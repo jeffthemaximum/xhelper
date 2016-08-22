bd303206d4b6dd4c2e419d5a2c670cad2822f80c
jeffthemaximum <frey.maxim@gmail.com>
2016-08-21 17:54:51 -0700

    finds emails, authors, and urls for wiley

--- xhelper.py
+++ xhelper.py
@@ -5,14 +5,22 @@ from oauth2client.service_account import ServiceAccountCredentials
 class Helpers:
 
     @classmethod
-    def find_email(cls, worksheets_list):
-        row_lowered = [cell.lower() for cell in worksheets_list]
-        return row_lowered.index('email') if 'email' in row_lowered else None
-
-    @classmethod
     def get_all_column_vals_as_row(cls, sheet, col_num):
         # get just elements at col_num index position
         return [el[col_num] for el in sheet.all_vals]
+
+    @classmethod
+    def get_column_number(cls, sheet, column_keyword):
+        '''
+        takes a sheet object
+        and a column keyword as string, such as 'email'
+        and returns the column number who's column title contains that string
+        else none if no column contains that string
+        '''
+        column_keyword = column_keyword.lower()
+        first_row_as_list = sheet.all_vals[0]
+        row_lowered = [cell.lower() for cell in first_row_as_list]
+        return row_lowered.index(column_keyword) if column_keyword in row_lowered else None
 
 
 class Xhelper:
@@ -47,13 +55,12 @@ class Wiley:
     def __init__(self, xhelper, sheet):
         self.xhelper = xhelper
         self.sheet = Sheet(self.xhelper, sheet)
-        self.email_col_num = self.get_email_column_number()
-
-    def get_email_column_number(self):
-        # get first row
-        first_row_as_list = self.sheet.all_vals[0]
-        # get column # of email
-        return Helpers.find_email(first_row_as_list)
+        self.email_col_num = Helpers.get_column_number(self.sheet, 'email')
+        self.url_col_num = Helpers.get_column_number(self.sheet, 'pageUrl')
+        self.author_col_num = Helpers.get_column_number(self.sheet, 'author')
+        self.all_emails = Helpers.get_all_column_vals_as_row(self.sheet, self.email_col_num)
+        self.all_urls = Helpers.get_all_column_vals_as_row(self.sheet, self.url_col_num)
+        self.all_authors = Helpers.get_all_column_vals_as_row(self.sheet, self.author_col_num)
 
     def wiley(self):
         # return and print error if 
@@ -64,9 +71,8 @@ class Wiley:
             return None
         # else get list of all emails
         else:
-            all_emails = Helpers.get_all_column_vals_as_row(self.sheet, self.email_col_num)
-            print all_emails
-            
+            pu.db
+            print self.all_emails
 
     def run(self):
         return self.wiley()
