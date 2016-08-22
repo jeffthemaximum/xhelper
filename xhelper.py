83eb9afb6489b7242d258c6b0fa0b2bbd811d8b5
jeffthemaximum <frey.maxim@gmail.com>
2016-08-22 16:29:40 -0400

    gets first name and writes to sheet

--- xhelper.py
+++ xhelper.py
@@ -45,6 +45,68 @@ class Helpers:
         row_lowered = [cell.lower() for cell in first_row_as_list]
         return row_lowered.index(column_keyword) if column_keyword in row_lowered else None
 
+    @classmethod
+    def get_last_col_with_text(cls, sheet):
+        '''
+        takes a sheet
+        and returns the last column number
+        that contains text
+        '''
+        counter = 0
+        total_cols = sheet.col_count
+        top_row = sheet.row_values(1)
+        for i, col_val in enumerate(top_row):
+            if len(col_val) != 0:
+                counter = i
+        return counter
+
+    @classmethod
+    def get_col_letter_from_number(cls, col_num):
+        '''
+        takes a number, like 3
+        and returns the column letter
+        like D
+        assumes zero indexed number
+        '''
+        return chr(ord('A') + col_num + 1)
+
+    @classmethod
+    def build_range_string(cls, col_letter, num_rows):
+        '''
+        takes a letter, like 'A'
+        and a number, like '15'
+        and build a range string
+        like 'A2:A17'
+        assumes u want to start at 2nd row
+        '''
+        start = col_letter + str(2)
+        end = col_letter + str(2 + num_rows)
+        return (start + ':' + end)
+
+    @classmethod
+    def and_jr_check(cls, name_string):
+        '''
+        takes a string, like 'jr', 'jr.', or 'maxim', or 'and'
+        returns true if it's 'maxim'
+        false if any form of 'jr' or 'and'
+        '''
+        if name_string.lower() == 'jr':
+            return False
+        elif name_string.lower() == 'jr.':
+            return False
+        elif name_string.lower() == 'and':
+            return False
+        else:
+            return True
+
+    @classmethod
+    def get_cell_list(cls, sheet, list_of_strs):
+        last_col_num = Helpers.get_last_col_with_text(self.sheet.sheet)
+        last_col_letter = Helpers.get_col_letter_from_number(last_col_num)
+        num_rows = len(first_names)
+        range_string = Helpers.build_range_string(last_col_letter, num_rows)
+        cell_list = self.sheet.sheet.range(range_string)
+        return cell_list
 
 class Xhelper:
     def __init__(self, json_file_name, spread_sheet_name):
@@ -126,51 +188,73 @@ class Wiley:
 
     def get_first_name_if_initial(self, name, idx=0):
         # catch case like ['T', 'Whitten']
+        # if self.all_urls[idx] == 'http://onlinelibrary.wiley.com/wol1/doi/10.1111/oik.01745/abstract':
+        #     pu.db
+        email = ''
         try:
             name[0]
         except:
             return ''
-        if len(name[0]) is 1:
-            soup = self.all_soups[idx]
-            correspondence = soup.soup.find("p", {"id": "correspondence"})
-            # try finding email
-            if correspondence.find('a') is not None:
-                email = correspondence.find('a').text.encode('ascii', 'ignore')
-                username = email.split('@')[0]
-                if '.' in username:
-                    first_name = username.split('.')[0]
-                    if len(first_name) > 1:
-                        return first_name
-                # handle case like ['E', 'J', 'Cartwright'] and email='ecartw2@emory.edu'
-                else:
-                    last_name = name[-1]
-                    correspondence_text = correspondence.text.encode('ascii', 'ignore')
-                    correspondence_text_list = ["".join([char for char in el if char.isalpha()]) for el in correspondence_text.split(' ')]
-                    if last_name in correspondence_text_list or last_name.title() in correspondence_text_list:
-                        last_name_idx = correspondence_text_list.index(last_name)
-                        first_name = correspondence_text_list[last_name_idx - 1]
-                        return first_name
+        if len(name[0]) == 1:
+            email = self.get_email(idx)
+            username = email.split('@')[0]
+            if '.' in username:
+                first_name = username.split('.')[0]
+                if len(first_name) > 1:
+                    return first_name
+            # handle case like ['E', 'J', 'Cartwright'] and email='ecartw2@emory.edu'
+            else:
+                last_name = name[-1] if Helpers.and_jr_check(name[-1]) else name[-2]
+                correspondence_text = correspondence.text.encode('ascii', 'ignore')
+                correspondence_text_list = ["".join([char for char in el if char.isalpha()]) for el in correspondence_text.split(' ')]
+                # if self.all_urls[idx] == 'http://onlinelibrary.wiley.com/wol1/doi/10.1111/oik.01745/abstract':
+                #     pu.db
+                if last_name in correspondence_text_list or last_name.title() in correspondence_text_list:
+                    last_name_idx = correspondence_text_list.index(last_name)
+                    correspondence_idx = correspondence_text_list.index('Correspondence') if 'Correspondence' in correspondence_text_list else None
+                    if correspondence_idx:
+                        name_after_correspondence = correspondence_text_list[correspondence_idx + 1]
+                        if name_after_correspondence == '':
+                            name_after_correspondence = correspondence_text_list[correspondence_idx + 2]
+                        return name_after_correspondence
                     else:
-                        # handle case like ['J', 'Sean', 'Doody'] and email='jseandoody@gmail.com'
-                        # and correspondence_text='*Corresponding author. E-mail: jseandoody@gmail.com'
-                        if len(name) is 3:
-                            middle_name = name[1]
-                            if len(middle_name) > 1:
-                                return middle_name
+                        first_name = correspondence_text_list[last_name_idx - 1] if len(correspondence_text_list[last_name_idx - 1]) != 1 else correspondence_text_list[last_name_idx - 2]
+                        return first_name
+                else:
+                    # handle case like ['J', 'Sean', 'Doody'] and email='jseandoody@gmail.com'
+                    # and correspondence_text='*Corresponding author. E-mail: jseandoody@gmail.com'
+                    if len(name) == 3:
+                        middle_name = name[1]
+                        if len(middle_name) > 1:
+                            return middle_name
         if len(name[0]) > 1:
             return name[0]
         else:
             print name[0]
+            print "URLURLURL" + self.all_urls[idx]
+            return ''
+            
 
 
     def get_first_names(self):
-
         names = [self.get_first_name_by_url(url) for url in self.all_urls]
-
         cleaned_names = [self.clean_author_email(name) for name in names]
-
         first_names = [self.get_first_name_if_initial(idx=i, name=name) for i, name in enumerate(cleaned_names)]
         return first_names
+
+    def get_email(self, idx):
+        soup = self.all_soups[idx]
+        correspondence = soup.soup.find("p", {"id": "correspondence"})
+        if correspondence is None:
+            correspondence = soup.soup.find("p", {"id": "contactDetails"})
+            email = correspondence.find("span", {"class": "email"}).text.split('(')[1].split(')')[0]
+        if email != '' or correspondence.find('a') is not None:
+            email = correspondence.find('a').text.encode('ascii', 'ignore') if email == '' else email
+        return email
+
+
+    def get_emails(self):
+        emails = [self.get_email(idx=i) for i, url in enumerate(self.all_soups)]
 
     def wiley(self):
         # return and print error if 
@@ -183,8 +267,17 @@ class Wiley:
         else:
             # TODO get first name
             first_names = self.get_first_names()
-            # TODO get email
+            cell_list = Helpers.get_cell_list(self.sheet.sheet, first_names)
+            for i, cell in enumerate(first_names):
+                cell_list[i].value = first_names[i].title()
+            self.sheet.sheet.update_cells(cell_list)
             print first_names
+            # TODO get email
+            emails = self.get_emails()
+            cell_list = Helpers.get_cell_list(self.sheet.sheet, emails)
+            self.sheet.sheet.update_cells(cell_list)
+            print emails
+
 
     def run(self):
         return self.wiley()
